fcb971ab4f0ecf8472fe0bd01f2538e027061328
iuliux1@gmail.com <iuliux1@gmail.com@402571a3-cc95-8c47-151b-162acd53266f>
2011-06-22 13:17:28 +0000

    Preferinte curatate + stuff
    git-svn-id: http://stickier.googlecode.com/svn/Stickier@19 402571a3-cc95-8c47-151b-162acd53266f

--- src/ro/pub/stickier/OverlayDrawer.java
+++ src/ro/pub/stickier/OverlayDrawer.java
@@ -20,7 +20,7 @@ public class OverlayDrawer extends View {
 		super(context, attrs);
 		paint = new Paint();
 		paint.setARGB(255, 10, 200, 10);
-		paint.setStrokeWidth(10.0f);
+		paint.setStrokeWidth(8.0f);
 		
 		pts = new ArrayList<Float>();
 	}
@@ -28,16 +28,41 @@ public class OverlayDrawer extends View {
 	@Override
 	public void onDraw(Canvas canvas) {
 		if(!pts.isEmpty()){
-			float[] ptsArray = new float[pts.size()];
+			float[] ptsArr = new float[pts.size()];
 	
+			
 			for (int i = 0; i < pts.size(); i++) {
 			    Float f = pts.get(i);
-			    ptsArray[i] = (f != null ? f : Float.NaN); // Or whatever default you want.
+			    ptsArr[i] = (f != null ? f : Float.NaN); // Or whatever default you want.
 			}
 			
-			canvas.drawPoints(ptsArray, paint);
-			Log.d(TAG, "Overlay ReDrawn!");
+			Log.d(TAG, "pts.size() = "+pts.size()); 
+			if(pts.size() == 6){
+				for (int i = 0; i < pts.size(); i++)
+					Log.d(TAG, "["+i+"] "+ptsArr[i]);
+				
+				final float d12 = distance(ptsArr[0], ptsArr[1], ptsArr[2], ptsArr[3]);
+				final float d23 = distance(ptsArr[2], ptsArr[3], ptsArr[4], ptsArr[5]);
+				final float d13 = distance(ptsArr[0], ptsArr[1], ptsArr[4], ptsArr[5]);
+				
+				Log.d(TAG, "d 1->2 "+d12);
+				Log.d(TAG, "d 2->3 "+d23);
+				Log.d(TAG, "d 1->3 "+d13);
+				
+				//TODO: calculare unghiuri si determinare pe cel de (cat mai) 90 grade
+				
+				canvas.drawLine(ptsArr[0], ptsArr[1], ptsArr[2], ptsArr[3], paint);
+				canvas.drawLine(ptsArr[0], ptsArr[1], ptsArr[4], ptsArr[5], paint);
+				canvas.drawLine(ptsArr[4], ptsArr[5], ptsArr[2], ptsArr[3], paint);
+			}else{
+				canvas.drawPoints(ptsArr, paint);
+				Log.d(TAG, "Overlay ReDrawn!");
+			}
 		}
+	}
+	
+	private float distance(float p1x, float p1y, float p2x, float p2y){
+		return (float)Math.sqrt((p1x - p1y)*(p1x - p1y) + (p2x - p2y)*(p2x - p2y));
 	}
 	
 	public void addPoint(float x, float y){
